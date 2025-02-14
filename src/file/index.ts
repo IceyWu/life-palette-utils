8e06091da19b7be8428cda5bea49a95ad92ac477
IceyWu <3128006406@qq.com>
2025-02-14 14:09:18 +0800

    fix: 调整图片解析逻辑

--- src/file/index.ts
+++ src/file/index.ts
@@ -1,45 +1,80 @@
-export interface Options {
-  resize?: string;
-  videoSnapshot?: string;
+import { customDestr, deepClone, getObjVal } from "@iceywu/utils";
+
+// 定义 Exif 数据的类型
+interface ExifData {
+  Make?: {
+    value: string;
+  };
 }
-const getObjVal = (obj: any, key: string, defaultValue?: string) => {
-  return obj && obj[key] ? obj[key] : defaultValue;
-};
+
+// 定义 data 对象的类型
+interface FileData {
+  url?: string;
+  type?: string;
+  videoSrc?: string | null;
+  cover?: string;
+  fromIphone?: boolean;
+  exif?: string;
+  [key: string]: any;
+}
+
+// 定义 options 对象的类型
+interface Options {
+  format?: string;
+  resize?: number;
+}
 
 /**
- * @description 返回文件的cover
- * @param options {Object} 配置参数
- * @returns {Object} 返回文件的cover对象
+ * 检查数据是否为苹果设备拍摄的图片
+ * @param data 包含图片信息的数据对象
+ * @returns 如果是苹果设备拍摄的图片返回 true，否则返回 false
  */
-export function fileParse(data: any, options?: Options): string {
-  const { fileType, file, cover } = data || {};
+export function isIphoneImg(data: FileData): boolean {
+  const exif = getObjVal(data, 'exif', '{}');
+  const exifData = customDestr(exif, { customVal: {} }) as ExifData;
+  return getObjVal(exifData, 'Make.value') === 'Apple';
+}
 
-  let addInfo = {};
-  if (fileType === "IMAGE") {
-    const resizeVal = getObjVal(options, "resize", "l_400");
-    let preSrc = `${file}?x-oss-process=image/resize,${resizeVal}`;
-    let src = `${file}`;
-    const fileSuffix = file.slice(Math.max(0, file.lastIndexOf(".")));
-    if (fileSuffix.toUpperCase() === ".HEIC") {
-      preSrc = `${file}?x-oss-process=image/resize,l_800/format,jpg`;
-      src = `${file}?x-oss-process=image/resize,l_800/format,jpg`;
+/**
+ * 返回文件的 cover
+ * @param data 包含文件信息的数据对象
+ * @param options 配置参数，包含 format 和 resize 选项
+ * @returns 处理后的文件信息对象
+ */
+export function fileParse(data: FileData, _options?: Options): FileData {
+  const { url = '', type = 'image/jpeg', cover = '', fromIphone } = data || {};
+  const fileType = type.toUpperCase().includes('VIDEO') ? 'VIDEO' : 'IMAGE';
+  const file = url;
+  const isIphone = isIphoneImg(data);
+  const addInfo: { baseSrc: string; thumbnailUrl: string, cover: string } = {
+    baseSrc: '',
+    thumbnailUrl: '',
+    cover: ''
+  };
+
+  const { format = 'jpg', resize = 400 } = _options || {};
+
+  if (fileType === 'IMAGE') {
+    let thumbnailUrl = `${file}?x-oss-process=image/resize,l_${resize}`;
+    let baseSrc = url;
+    const fileSuffix = file.slice(Math.max(0, file.lastIndexOf('.'))).toLowerCase();
+    if ((['.heic', '.HEIC'].includes(fileSuffix) || isIphone || fromIphone) && !file.includes(`format,${format}`)) {
+      baseSrc = `${url}?x-oss-process=image/format,${format}`;
+      thumbnailUrl = `${file}?x-oss-process=image/resize,l_800/format,${format}`;
     }
-    addInfo = {
-      src,
-      preSrc,
-    };
-  } else if (fileType === "VIDEO") {
-    const videoSnapshot = getObjVal(options, "videoSnapshot", "t_7000");
-    const srcT =
-      cover ||
-      `${file}?x-oss-process=video/snapshot,${videoSnapshot},f_jpg,w_0,h_0,m_fast`;
-    addInfo = {
-      src: srcT,
-      preSrc: srcT,
-    };
+    addInfo['baseSrc'] = baseSrc;
+    addInfo['thumbnailUrl'] = thumbnailUrl;
+  } else if (fileType === 'VIDEO') {
+    const coverUrl = cover || `${url}?x-oss-process=video/snapshot,t_7000,f_${format},w_0,h_0,m_fast`;
+
+    addInfo['cover'] = coverUrl;
   }
-  return {
+
+  const baseData: FileData & { fileType: string } = {
     ...data,
     ...addInfo,
+    fileType,
   };
+
+  return baseData;
 }
